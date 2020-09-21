4dcfdaf71ff8ff79ef3a83c47d5840a73d99cad2
s-plotnikov <seryoga.22@mail.ru>
2020-09-21 21:40:44 +0700

    added animation

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,9 @@ $(function(){
     $('.slider__inner').slick({
         dots: false,
         infinite: true,
-        speed: 300,
+        speed: 1000,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 4000,
         slidesToShow: 1,
         adaptiveHeight: true,
         arrows: true,
@@ -52,5 +52,22 @@ $(function(){
           $('#'+id).addClass('active-tab').fadeIn();
           return false;
         });
+
+        var wow = new WOW(
+          {
+            boxClass:     'wow',      // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset:       0,          // distance to the element when triggering the animation (default is 0)
+            mobile:       true,       // trigger animations on mobile devices (default is true)
+            live:         true,       // act on asynchronously loaded content (default is true)
+            callback:     function(box) {
+              // the callback is fired every time an animation is started
+              // the argument that is passed in is the DOM node being animated
+            },
+            scrollContainer: null,    // optional scroll container selector, otherwise use window,
+            resetAnimation: true,     // reset animation on end (default is true)
+          }
+        );
+        wow.init(); 
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,7 @@ gulp.task('css', function(){
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/animate.css/animate.css'
   ])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('app/scss'))
@@ -46,6 +47,7 @@ gulp.task('script', function(){
 gulp.task('js', function(){
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/wow.js/dist/wow.js'
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
